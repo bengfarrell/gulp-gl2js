70577e1fef291f0e7d8fa1a6eac4c564a2a3ab9b
bengfarrell <ben@benfarrell.com>
2015-12-13 00:13:21 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var through = require('through2');
+var path = require('path');
+var gutil = require('gulp-util');
+var PluginError = gutil.PluginError;
+var File = gutil.File;
+
+module.exports = function(outfile, opt) {
+    var joinedShaders = '';
+    var shaders = {};
+
+    if (!opt) {
+        opt = {}
+    }
+
+    if (!opt.assignto) {
+        opt.assignto = 'x';
+    }
+
+    if (!outfile) {
+       outfile = 'shaders';
+    }
+
+    function makeJS(file, enc, cb) {
+        var shadertxt = String(file.contents);
+        shadertxt = shadertxt.replace(/\r?\n/g, ' ');
+        shadertxt = shadertxt.replace(/\t/g, ' ');
+
+        var filename = path.basename(file.path, '.glsl');
+        //shadertxt = opt.namespace + "." + filename + " = '" + shadertxt + "';";
+
+        var shadername;
+        var shadertype;
+        if (filename.indexOf('.vertex') !== -1) {
+            shadername = filename.substr(0, filename.indexOf('.vertex'));
+            shadertype = 'vertex';
+        } else if (filename.indexOf('.fragment') !== -1) {
+            shadername = filename.substr(0, filename.indexOf('.fragment'));
+            shadertype = 'fragment';
+        }
+
+        if (!shaders[shadername]) {
+            shaders[shadername] = {};
+        }
+        shaders[shadername][shadertype] = shadertxt;
+        cb();
+    };
+
+    function endStream(cb) {
+        var file = new gutil.File({
+            base: path.join(__dirname, './'),
+            cwd: __dirname,
+            path: path.join(__dirname, './' + outfile + '.json')
+        });
+
+        file.contents = new Buffer(JSON.stringify(shaders, null, 2));
+        this.push(file);
+
+        var file = new gutil.File({
+            base: path.join(__dirname, './'),
+            cwd: __dirname,
+            path: path.join(__dirname, './' + outfile + '.js')
+        });
+
+        file.contents = new Buffer(opt.assignto + ' = ' + JSON.stringify(shaders, null, 2) + ';');
+        this.push(file);
+        cb();
+    }
+
+
+    return through.obj(makeJS, endStream);
+};
